591f3659951e82f1c3f26e054941d20d8b4ddf9c
Christian Jensen <cbejensen@gmail.com>
2018-11-10 00:01:35 -0700

    restructured app root

---
+++ src/components/Paint.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Name from './Name'
+import Coords from './Coords'
+import ColorPicker from './ColorPicker'
+import Canvas from './Canvas'
+
+export default class Paint extends React.Component {
+  constructor(props) {
+    super(props)
+    this.headerRef = React.createRef()
+    this.state = {
+      name: '',
+      activeColorIndex: 0,
+      mouseCoords: [0, 0]
+    }
+  }
+  componentDidMount() {
+    this.setDocumentTitle(this.state.name)
+    document.addEventListener('mousemove', this.handleMouseMove)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.name !== prevState.name) {
+      this.setDocumentTitle(this.state.name)
+    }
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this.handleMouseMove)
+  }
+  handleMouseMove = e => {
+    this.setState({
+      mouseCoords: [e.pageX, e.pageY]
+    })
+  }
+  setDocumentTitle = name => {
+    document.title = `EZ Paint - ${name || 'Untitled'}`
+  }
+  render() {
+    const activeColor = this.props.colors[this.state.activeColorIndex]
+    // canvas Y coordinate needs to be adjusted to
+    // account for header height
+    let canvasYCoord = this.state.mouseCoords[1]
+    if (this.headerRef && this.headerRef.current) {
+      // if we have the ref to the header element
+      canvasYCoord -= this.headerRef.current.offsetHeight
+    }
+    return (
+      <div className="app">
+        <header ref={this.headerRef}>
+          <div className="top-bar">
+            <Name
+              name={this.state.name}
+              handleChange={val => this.setState({ name: val })}
+            />
+            {canvasYCoord >= 0 && (
+              <Coords x={this.state.mouseCoords[0]} y={canvasYCoord} />
+            )}
+          </div>
+          <ColorPicker
+            colors={this.props.colors}
+            activeColor={activeColor}
+            handleChange={i => this.setState({ activeColorIndex: i })}
+          />
+        </header>
+        <Canvas color={activeColor} mouseCoords={this.state.mouseCoords} />
+      </div>
+    )
+  }
+}
